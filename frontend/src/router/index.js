9ae53f15ba60b594f7a058e98181532032517720
zo-ko <3087789206@qq.com>
2026-02-22 21:35:21 +0800

    完成更新角色前端

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -8,6 +8,7 @@ import RegisterIndex from "@/views/user/account/RegisterIndex.vue";
 import SpaceIndex from "@/views/user/space/SpaceIndex.vue";
 import ProfileIndex from "@/views/user/profile/ProfileIndex.vue";
 import {useUserStore} from "@/stores/user.js";
+import UpdateCharacter from "@/views/create/character/UpdateCharacter.vue";
 
 
 const router = createRouter({
@@ -33,6 +34,14 @@ const router = createRouter({
       path: '/create/',
       component: CreateIndex,
       name: 'create-pageindex',
+      meta: {
+        needLogin: true,
+      },
+    },
+    {
+      path: '/create/character/update/:character_id/',
+      component: UpdateCharacter,
+      name: 'update-character',
       meta: {
         needLogin: true,
       },
