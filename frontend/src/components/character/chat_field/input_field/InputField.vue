9d768ec5c4d51963e8b95ccffdf779684e6be4ae
zo-ko <3087789206@qq.com>
2026-03-02 20:01:11 +0800

    实现聊天框

---
+++ frontend/src/components/character/chat_field/input_field/InputField.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+import SendIcon from "@/components/character/icons/SendIcon.vue";
+import MicIcon from "@/components/character/icons/MicIcon.vue";
+</script>
+
+<template>
+  <div class="absolute bottom-4 left-2 h-12 w-86 flex items-center">
+    <input class="input bg-black/30 text-white text-base backdrop-blur-sm w-full h-full rounded-2xl pr-20"
+            type="text"
+            placeholder="文本输入......"
+    >
+    <div class="absolute right-2 w-8 h-8 flex justify-center items-center cursor-pointer">
+      <SendIcon/>
+    </div>
+    <div class="absolute right-8 w-8 h-8 flex justify-center items-center cursor-pointer">
+      <MicIcon/>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
